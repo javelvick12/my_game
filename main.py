070f14fba70780ba59ce40ce52c6bb0ed292f034
Jason Velvick <jason.velvick@du.edu>
2025-07-18 18:09:57 -0600

    7.18 update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sys import exit
 import os
 
 class Character:
-    def __init__(self):
+    def __init__(self, character):
         character.color = "red"
         character.size = "regular"
         character.speed = "fast"
@@ -11,6 +11,7 @@ class Character:
 
 
 def main():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
     #Screen Parameters
     SCREEN_WIDTH = 800 
     SCREEN_HEIGHT = 600
@@ -19,9 +20,9 @@ def main():
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pg.display.set_caption('Color Man')
     clock = pg.time.Clock()
-    my_font = pg.font.Font('assets/Minecraft.ttf', 48)
-    sky_image = pg.image.load('assets/sky.jpg')
-    ground_image = pg.image.load('assets/ground.jpg')
+    my_font = pg.font.Font(os.path.join(current_dir, 'assets/Minecraft.ttf'), 48)
+    sky_image = pg.image.load('assets/sky.jpg').convert()
+    ground_image = pg.image.load('assets/ground.jpg').convert()
     sky_height = SCREEN_HEIGHT * 3 // 4
     sky_scaled = pg.transform.scale(sky_image, (SCREEN_WIDTH, sky_height))
     ground_height = SCREEN_HEIGHT - sky_height
@@ -30,10 +31,12 @@ def main():
     start_object = my_font.render('Start', False, 'Orange')
     options_object  = my_font.render('Options', False, 'Orange')
     exit_object = my_font.render('Exit', False, 'Orange')
-    snail_object = pg.image.load('Documents/python_games/my_game/assets/snail/snail1.png')
-    snail_move = pg.image.load('Documents/python_games/my_game/')
-    snail_x_pos = 600
-    #test_surface.fill('Red')
+    snail_object = pg.image.load(os.path.join(current_dir, 'assets/snail/snail1.png')).convert_alpha() #snail frame 1
+    snail_move = pg.image.load(os.path.join(current_dir, 'assets/snail/snail2.png')).convert_alpha() #snail frame 2
+    player_surface = pg.image.load(os.path.join(current_dir, 'assets/player/player_walk_1.png')).convert_alpha()
+    player_rect = player_surface.get_rect(midbottom = (100, sky_height)) #get rekt
+    snail_rect = snail_object.get_rect(midbottom = (600, sky_height))  #get rekt
+    #snail_rect2 = snail_move.get_rect(midbottom = (600, sky_height))
     #The main run loop for the game
     while run:
         pg.display.update()
@@ -45,10 +48,14 @@ def main():
         screen.blit(ground_scaled,(0,sky_height))
         screen.blit(title_object,(275, 50))
         screen.blit(start_object, (275, 100))
-        if snail_x_pos <= -90:
-            snail_x_pos = 765
-        snail_x_pos -= 1
-        screen.blit(snail_object, (snail_x_pos, 420))
+        snail_rect.x -= 1
+        if snail_rect.left <= -90:
+            snail_rect.x = 750
+        if snail_rect.x % 50 == 0:
+            screen.blit(snail_object, snail_rect)
+        else:
+            screen.blit(snail_move, snail_rect)
+        screen.blit(player_surface, player_rect)
     pg.quit()
     exit()
 
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 import os
 
-
+for i in range(750, -90, -1):
+    if i % 2 == 0:
+        print(i)
